c991b47077ee12467bde7bb3f76e8fb41eaf7e7c
Guillaume Kehren <kehren.guillaume@gmail.com>
2022-03-11 23:16:20 +0100

    memset

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkehren <gkehren@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/11 18:05:46 by gkehren           #+#    #+#             */
+/*   Updated: 2022/03/11 23:14:30 by gkehren          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+void    *ft_memset(void *s, int c, size_t n);
+
+#endif
