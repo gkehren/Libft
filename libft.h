40f1d56718e790774472b845d246fc414471ecc2
Guillaume Kehren <kehren.guillaume@gmail.com>
2022-03-13 16:46:04 +0100

    strcmp, strncmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gkehren <gkehren@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 18:05:46 by gkehren           #+#    #+#             */
-/*   Updated: 2022/03/13 16:33:35 by gkehren          ###   ########.fr       */
+/*   Updated: 2022/03/13 16:42:24 by gkehren          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		strcmp(const char *s1, const char *s2);
+int		strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
