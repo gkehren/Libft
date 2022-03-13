e9dd4f3e753b6f94090d5e698940c65180b438ea
Guillaume Kehren <kehren.guillaume@gmail.com>
2022-03-13 16:31:42 +0100

    strstr

---
+++ ft_strstr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkehren <gkehren@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/13 16:25:59 by gkehren           #+#    #+#             */
+/*   Updated: 2022/03/13 16:31:06 by gkehren          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int		i;
+	int		j;
+	char	*pt;
+
+	i = 0;
+	pt = 0;
+	if (needle[i] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			pt = (char *)haystack + i;
+			j = 0;
+			while (haystack[i + j] == needle[j])
+			{
+				if (needle[j + 1] == '\0')
+					return (pt);
+				j++;
+			}
+			pt = 0;
+		}
+		i++;
+	}
+	return (0);
+}
